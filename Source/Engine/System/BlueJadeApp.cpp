e6f637a7c8f4419fe0af55152eeda8b121138e3d
jose <josmontenegro2012@gmail.com>
2018-11-09 02:04:35 -0500

    -Added Window Creation and Input Manager

--- Source/Engine/System/BlueJadeApp.cpp
+++ Source/Engine/System/BlueJadeApp.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <direct.h>
 #include <string.h>
+#include <tchar.h>
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
-bool BlueJadeApp::InitInstance()
+bool BlueJadeApp::InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	if (!IsOnlyInstance("BlueJadeWindow")) {
 		cout << "Another instance of the game is running. Exiting..." << endl;
@@ -27,6 +30,10 @@ bool BlueJadeApp::InitInstance()
 
 	cout << "CPU Architecture: " << GetCPUArchitecture() << endl;
 	cout << "CPU Speed: " << GetCPUSpeed() << " MHz" << endl;
+
+	if (!InitializeWindow(hInstance, nCmdShow)) {
+		return false;
+	}
 
 	return true;
 }
@@ -149,6 +156,92 @@ LPCTSTR BlueJadeApp::GetCPUArchitecture()
 	}
 }
 
+LRESULT CALLBACK BlueJadeApp::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		TextOut(hdc,
+			5, 5,
+			windowMessage, _tcslen(windowMessage));
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_LBUTTONDOWN:
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+bool BlueJadeApp::InitializeWindow(HINSTANCE hInstance, int nCmdShow) {
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = this->WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return false;
+	}
+
+	m_hInst = hInstance;
+
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return false;
+	}
+
+	ShowWindow(hWnd,
+		nCmdShow);
+	UpdateWindow(hWnd);
+
+	return true;
+}
+
 void BlueJadeApp::CloseApp()
 {
 	if (m_MutexHandle) {
@@ -157,6 +250,13 @@ void BlueJadeApp::CloseApp()
 	}
 }
 
-void BlueJadeApp::MainLoop() {
-	Sleep(10000);
+int BlueJadeApp::MainLoop() {
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
 }
--- Source/Engine/System/BlueJadeApp.h
+++ Source/Engine/System/BlueJadeApp.h
@@ -1,16 +1,24 @@
-#include <Windows.h>
+#include <windows.h>
+#include <tchar.h>
 
 #define DEFAULT_STORAGE_NEEDED 314572800
 #define MAX_CONTIGUOUS_MEMORY_NEEDED 314572800
+
+static TCHAR szWindowClass[] = _T("BlueJadeApp");
+static TCHAR szTitle[] = _T("Blue Jade Engine Game");
+static TCHAR windowMessage[] = _T("Hello World!");
 
 class BlueJadeApp {
 public:
 	//Members
 
 	//Methods
-	bool InitInstance();
+	bool InitInstance(HINSTANCE hInstance, int nCmdShow);
 	void CloseApp();
-	void MainLoop();
+	int MainLoop();
+
+	//Window message handler
+	static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 private:
 	/****
@@ -19,6 +27,9 @@ private:
 
 	//The mutex handle for this process
 	HANDLE m_MutexHandle;
+
+	//The current hinstance
+	HINSTANCE m_hInst;
 
 	/****
 	Methods
@@ -38,4 +49,7 @@ private:
 
 	//Return cpu architecture
 	LPCTSTR GetCPUArchitecture();
+
+	//Create a new window
+	bool InitializeWindow(HINSTANCE hInstance, int nCmdShow);
 };
